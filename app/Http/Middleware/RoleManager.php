c9e65d985ad91aa8ab6932ab6316383fb6fc0059
Suhit <thepowerhouse400@gmail.com>
2026-05-04 00:50:27 +0545

    Project Initialized - Laravel Installed - Breeze Installed - Added Role Based Basic Auth

---
+++ app/Http/Middleware/RoleManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $roles = [
+            0 => 'admin',
+            1 => 'vendor',
+            2 => 'user',
+        ];
+
+        $userRole = $roles[Auth::user()->role] ?? null;
+
+        if ($userRole === $role) {
+            return $next($request);
+        }
+
+        abort(403);
+    }
+}
