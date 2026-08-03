3a7fbe078c884bddaf3987870cc19c7ebf34be0d
Suhit <thepowerhouse400@gmail.com>
2026-08-03 22:53:17 +0545

    User Dashboard (Stats)

--- app/Http/Controllers/Customer/CustomerPageController.php
+++ app/Http/Controllers/Customer/CustomerPageController.php
@@ -12,11 +12,6 @@ use App\Models\Order;
 class CustomerPageController extends Controller
 {
 
-    public function stats()
-    {
-        return view('customer.stats.manage');
-    }
-
     public function orders(Request $request)
     {
         $search = $request->query('search');
@@ -87,5 +82,113 @@ class CustomerPageController extends Controller
             ->withQueryString();
 
         return view('customer.donations.manage', compact('orders'));
+    }
+
+    
+    public function stats()
+    {
+        $userId = Auth::id();
+        $orders = Order::where('user_id', $userId)
+            ->with([
+                'user',
+                'items.product'
+            ])
+            ->get();        
+            
+            // Calculations
+            $donation = $orders
+                ->where('status', '!=', 'cancelled')
+                ->pluck('items')
+                ->flatten()
+                ->where('donation_quantity', '>', 0)
+                ->sum(function ($item) {
+                    return (($item->total_price / $item->quantity) * $item->donation_quantity);
+                });
+                
+            // Orders
+            $order_month = 
+                $orders->where('status', '!=', 'cancelled')
+                ->whereBetween('created_at', [
+                    now()->startOfMonth(),
+                    now()->endOfMonth(),
+                ])
+                ->count();
+            
+            // Ordered Amount
+            $amount_month = $orders
+                ->where('status', '!=', 'cancelled')
+                ->whereBetween('created_at', [
+                    now()->startOfMonth(),
+                    now()->endOfMonth(),
+                ])
+                ->sum('total_cost');
+            
+            $amount_prev = $orders
+                ->where('status', '!=', 'cancelled')
+                ->whereBetween('created_at', [
+                    now()->subMonth()->startOfMonth(),
+                    now()->subMonth()->endOfMonth(),
+                ])
+                ->sum('total_cost');
+
+            $amount_var = $amount_prev > 0 ? (($amount_month - $amount_prev) * 100) / ($amount_prev) : null;
+
+            // Donation Amount
+            $donation_month = $orders
+                ->where('status', '!=', 'cancelled')
+                ->whereBetween('created_at', [
+                    now()->startOfMonth(),
+                    now()->endOfMonth(),
+                ])
+                ->pluck('items')
+                ->flatten()
+                ->where('donation_quantity', '>', 0)
+                ->sum(function ($item) {
+                    return (($item->total_price / $item->quantity) * $item->donation_quantity);
+                });
+            
+            $donation_prev = $orders
+                ->where('status', '!=', 'cancelled')
+                ->whereBetween('created_at', [
+                    now()->subMonth()->startOfMonth(),
+                    now()->subMonth()->endOfMonth(),
+                ])
+                ->pluck('items')
+                ->flatten()
+                ->where('donation_quantity', '>', 0)
+                ->sum(function ($item) {
+                    return (($item->total_price / $item->quantity) * $item->donation_quantity);
+                });
+
+            $donation_var = $donation_prev > 0 ? (($donation_month - $donation_prev) * 100) / ($donation_prev) : null;
+
+
+            // Stats
+            $stats = [
+                'orders' => $orders->where('status', '!=', 'cancelled')->count(),
+                'order_amount' => $orders->where('status', '!=', 'cancelled')->sum('total_cost'),
+                'stores' => $orders->where('status', '!=', 'cancelled')
+                    ->pluck('items')
+                    ->flatten()
+                    ->pluck('product.vendor_id')
+                    ->unique()
+                    ->count(),
+                'products' => $orders->where('status', '!=', 'cancelled')
+                    ->pluck('items')
+                    ->flatten()
+                    ->pluck('product')
+                    ->unique()
+                    ->count(),
+                'donation' => $donation,
+            ];
+
+            // Per Month Comparsion
+            $analysis = [
+                'orders' => $order_month,
+                'donation' =>$donation_var,
+                'amount' =>$amount_var,
+            ];
+        
+        return view('customer.stats.manage', compact('stats', 'analysis'));
     }
 }
--- resources/views/customer/stats/manage.blade.php
+++ resources/views/customer/stats/manage.blade.php
@@ -3,5 +3,174 @@
     Stats - Customer
 @endsection
 @section('customer_content')
-    <h1>Customer Stats</h1>
+    <div class="">
+        <div class="container-fluid p-0">
+
+            <h2 class="text-center mb-5">Your Statistics</h2>
+
+            <div class="row">
+                <div class="col-xl-8 d-flex">
+                    <div class="w-100">
+                        <div class="row">
+
+                            {{-- Products --}}
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mt-0">
+                                                <h5 class="card-title">Items Purchased</h5>
+                                            </div>
+
+                                            <div class="col-auto">
+                                                <div class="stat text-primary">
+                                                    <i class="fa-solid fa-plate-wheat"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <h2 class="mt-1 mb-3">{{ $stats['products'] }}</h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Stores --}}
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mt-0">
+                                                <h5 class="card-title">Stores Explored</h5>
+                                            </div>
+
+                                            <div class="col-auto">
+                                                <div class="stat text-primary">
+                                                    <i class="fa-solid fa-shop"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <h2 class="mt-1 mb-3">{{ $stats['stores'] }}</h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Order --}}
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mt-0">
+                                                <h5 class="card-title">Successful Orders</h5>
+                                            </div>
+
+                                            <div class="col-auto">
+                                                <div class="stat text-primary">
+                                                    <i class="fa-solid fa-basket-shopping"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <h2 class="mt-1 mb-3">{{ $stats['orders'] || '0' }}</h2>
+                                        <div class="mb-0">
+                                            <small class="text-success">
+                                                +{{ $analysis['orders'] }}
+                                            </small>
+                                            <small class="text-muted">this month</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Ordered Amount --}}
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mt-0">
+                                                <h5 class="card-title">Ordered Amount</h5>
+                                            </div>
+
+                                            <div class="col-auto">
+                                                <div class="stat text-primary">
+                                                    <i class="fa-solid fa-dollar-sign"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <h2 class="mt-1 mb-3">Rs {{ $stats['order_amount'] }}</h2>
+                                        <div class="mb-0">
+                                            @if ($analysis['amount'])
+                                                <small
+                                                    class="{{ $analysis['amount'] > 0 ? 'text-success' : 'text-danger' }}">
+                                                    {{ round($analysis['amount'], 2) }}%
+                                                    <small class="text-muted">than last month</small>
+                                                </small>
+                                            @else
+                                                <small class="text-success">New</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Donated Amount --}}
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col mt-0">
+                                                <h5 class="card-title">Donated Amount</h5>
+                                            </div>
+
+                                            <div class="col-auto">
+                                                <div class="stat text-primary">
+                                                    <i class="fa-solid fa-gift"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <h2 class="mt-1 mb-3">Rs. {{ $stats['donation'] }}</h2>
+                                        <div class="mb-0">
+                                            @if ($analysis['donation'])
+                                                <small
+                                                    class="{{ $analysis['donation'] > 0 ? 'text-success' : 'text-danger' }}">
+                                                    {{ round($analysis['donation'], 2) }}%
+                                                    <small class="text-muted">than last month</small>
+                                                </small>
+                                            @else
+                                                <small class="text-success">New</small>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-xl-4">
+                    <div class="card flex-fill">
+                        <div class="card-header">
+                            <h5 class="card-title mb-0">Calendar</h5>
+                        </div>
+                        <div class="card-body d-flex">
+                            <div class="align-self-center w-100">
+                                <div class="chart">
+                                    <div id="datetimepicker-dashboard"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            flatpickr("#datetimepicker-dashboard", {
+                inline: true,
+                defaultDate: new Date()
+            });
+        });
+    </script>
 @endsection
